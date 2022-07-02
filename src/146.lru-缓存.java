ad8fc9bcc57b7ad4105148a7483965a9d7619375
Jiadi Su <20302010043@fudan.edu.cn>
2022-07-02 21:00:21 +0800

    LRU缓存

--- src/146.lru-缓存.java
+++ src/146.lru-缓存.java
@@ -1,3 +1,6 @@
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 /*
  * @lc app=leetcode.cn id=146 lang=java
  *
@@ -5,18 +8,30 @@
  */
 
 // @lc code=start
-class LRUCache {
+class LRUCache extends LinkedHashMap<Integer, Integer>{
+    private int capacity;
 
+    /**
+     * LRU缓存机制，可通过哈希表辅以双向链表实现
+     * @param capacity
+     */
+    
     public LRUCache(int capacity) {
-
+        super(capacity, 0.75F, true);
+        this.capacity = capacity;
     }
-    
+
     public int get(int key) {
-
+        return super.getOrDefault(key, -1);
     }
-    
-    public void put(int key, int value) {
 
+    public void put(int key, int value) {
+        super.put(key, value);
+    }
+
+    @Override
+    protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
+        return size() > capacity; 
     }
 }
 
